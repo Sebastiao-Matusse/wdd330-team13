ea699a69a38a1304c585b0a8370f8273129fff37
Sebastiao Matusse <matussevalete@gmail.com>
2026-07-15 21:03:18 +0200

    week02 team activity done

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,22 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []
-  cartItems.push(product);
+const product = await dataSource.findProductById(productId, dataSource)
+product.init();
+console.log(product)
+
+
+function addProductToCart() {
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(this.product);
   setLocalStorage("so-cart", cartItems);
 }
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
