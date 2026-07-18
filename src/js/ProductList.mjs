a2183575d47c532ef8e55f1b71f2f44219857912
Sebastiao Matusse <matussevalete@gmail.com>
2026-07-18 18:28:35 +0200

    week 02 Individual activity: Dynamic Product List done

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,46 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+        <a href="product_pages/?product=${product.Id}">
+            <img
+                src="${product.Image}"
+                alt="${product.Name}"
+            />
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.Name}</h2>
+            <p class="product-card__price">${product.FinalPrice}</p>
+        </a>
+    </li>`
+}
+
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        this.products = await this.dataSource.getData();
+        // console.log(this.products)
+        this.renderList()
+    }
+
+    renderList() {
+        renderListWithTemplate(productCardTemplate, this.listElement, this.products)
+    }
+
+}
+
+
+// Sebastiao Matusse's version
+// before implementing the randerWithList template function in the utils
+//
+// renderList() {
+//     this.listElement.innerHTML = "";
+//     // console.log(this.products);
+//     const html = this.products.map((product) => productCardTemplate(product));
+//     this.listElement.innerHTML = html.join("")
+// }
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,13 @@ export function getParam(param) {
   const product = urlParams.get('product');
   return product
 }
+
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  };
+  const htmlStrings = list.map(templateFn);
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+
+}
